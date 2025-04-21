c8b021a98ce333e0c174460157be6ba44a46a2aa
Sarah Lash <sarah.lash92@gmail.com>
2025-04-21 18:17:09 +0200

    [BE]-installing cors

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,8 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 const app: Express = express();
+const cors = require("cors");
+app.use(cors({ origin: "http://localhost:5173" }));
 const port = 8080;
 
 app.use(express.json());
