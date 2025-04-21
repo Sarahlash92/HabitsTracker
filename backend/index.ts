88d76781f9bf46b0b950e6d3a0f590d42740d90f
Sarah Lash <sarah.lash92@gmail.com>
2025-04-21 09:05:02 +0200

    [BE]-writing first get and mocking Data

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,32 @@
 import "reflect-metadata";
 import express, { Request, Response, Express, response } from "express";
+import path from "path";
+import fs from 'fs/promises';
 
 const app: Express = express();
 const port = 8080;
+const FILE_PATH = path.resolve(__dirname, './data/habits.json');
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('We have something')
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const habits = await readData();
+    res.json(habits)
+
+  } catch(error) {
+    res.status(500).json({ error: "Failed to read habits" });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port: http://localhost:${port}`);
 });
+
+const readData = async (): Promise<any[]> =>  {
+try {
+    console.log('FILE_PATH', FILE_PATH);
+    const data = await fs.readFile(FILE_PATH, 'utf-8');
+    return JSON.parse(data);
+  } catch {
+    return [];
+  }
+}
